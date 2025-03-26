944a8fa70983a9227f06ad29a23c68577bbe5cd4
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-26 16:33:35 -0300

    1, 2, 3 PROBANDO

---
+++ Curso C# - Nivel 2/ejemplo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            // tipo de datos elementales -> int - float - bool - char
+            // otros tipos de datos -> double - decimal - long - short - string - datetime
+
+            int a, b, c;
+            float d, f;
+
+            Console.WriteLine("Hola Como va?");
+            Console.WriteLine("Ingrese un numero: ");
+            
+            a = int.Parse(Console.ReadLine());
+            b = 10;
+            c = a + b;
+
+            Console.WriteLine("El resultado es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
