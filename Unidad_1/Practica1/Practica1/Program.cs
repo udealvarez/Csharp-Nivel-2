aeee649e45b083adc7aa058a87fd3e5c5b50d3be
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-27 13:56:27 -0300

    Practica 1 | Hola Mundo por consola

---
+++ Unidad_1/Practica1/Practica1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un proyecto de consola .Net Framework y ejecutar el "Hola Mundo".
+
+            Console.WriteLine("Hola mundo, estamos en el nivel 2!");
+            Console.ReadKey();
+        }
+    }
+}
