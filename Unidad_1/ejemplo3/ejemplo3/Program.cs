66fadd3f8bebd300a44ace1f826f571d40ea36d1
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-27 12:01:20 -0300

    Ejercicio 3 | Unidad 1

---
+++ Unidad_1/ejemplo3/ejemplo3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ciclos -> for - while - do while
+
+            /* OPERADORES
+             *      ++ -> INCREMENTO
+             *      -- -> DECRECIMIENTO
+             *      += -> ACUMULADOR
+             *      -+ -> DISMINUCION
+             *      *= -> hace lo mismo que el acumulador
+             *      /+ -> hace lo mismo que el acumulador
+             */
+
+            int a = 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                while (a != 0)
+                {
+                    Console.WriteLine("Hola " + a);
+                    a--;
+                }                
+            }
+            Console.ReadKey();
+
+
+            do
+            {
+                // codigo
+                // codigo
+                // codigo
+
+            } while (a != 0);
+        }
+    }
+}
