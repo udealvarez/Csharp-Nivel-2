a9c6b7a91c5d5892d076f2a658a0e46b1822a3b7
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-27 11:38:13 -0300

    Agrego Unidad 1 con ejercicios organizados

---
+++ Unidad_1/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            // tipo de datos elementales -> int - float - bool - char
+            // otros tipos de datos -> double - decimal - long - short - string - datetime
+
+            int a, b, c;
+            float d, f;
+
+            Console.WriteLine("Hola Como va?");
+            Console.WriteLine("Ingrese un numero: ");
+
+            a = int.Parse(Console.ReadLine());
+            b = 10;
+            c = a + b;
+
+            Console.WriteLine("El resultado es: " + c);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Unidad_1/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Condicionales
+
+            // IF
+            // SWITCH
+
+            /* OPERADORES RELACIONALES PARA EL IF
+             *      == -> IGUALDAD
+             *      != -> DISTINTO
+             *      >  -> MAYOR
+             *      <  -> MENOR
+             *      >= -> MAYOR O IGUAL
+             *      <= -> MENOR O IGUAL         
+             */
+
+            /* OPERADORES LOGICOS
+             *      !  -> NEGADO
+             *      && -> AND | Y
+             *      || -> OR | O 
+             * 
+             */
+
+
+            int a = 10, b = 12;
+
+            if (a == b && b != 10 || (a == 20))
+            {
+                // todo lo que necesite...
+
+                Console.WriteLine("se cumple..");
+
+                if (a != 9)
+                {
+                    // otra condicion
+                }
+            }
+            else if (a == 15)
+            {
+                switch (a)
+                {
+                    case 1:
+                        // instrucciones
+                        break;
+                    case 2:
+                        // instrucciones
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (b != a)
+            {
+
+            }
+            else if (b < 10)
+            {
+
+            }
+            else
+            {
+                // bloque opcional 
+            }
+
+        }
+    }
+}
