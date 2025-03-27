e58fb7d47a15ef032a77e713b0ba97ccb930ba6e
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-27 13:26:33 -0300

    Ejemplo 4 | Repaso | Unidad 1

---
+++ Unidad_1/ejemplo4/ejemplo4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vectores -> int - float - char - bool
+
+            int[] numeros = new int[10];    // esta es la manera de escribir un arreglo
+
+            numeros[0] = 10;
+            numeros[6] = 7;
+
+            int j = numeros[5];
+
+
+
+        }
+    }
+}
