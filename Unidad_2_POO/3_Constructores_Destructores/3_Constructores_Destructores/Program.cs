f9fbbe3cddc1046212ff5a4cf4fe8a342f215e80
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-28 14:37:44 -0300

    Constructores y Destructores

---
+++ Unidad_2_POO/3_Constructores_Destructores/3_Constructores_Destructores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Constructores_Destructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona personaUno = new Persona();
+            personaUno.setEdad(20);
+
+            // ENCAPSULAMIENTO: define que los atributos de una clase tienen que estar ocultos al exterior
+
+            Console.WriteLine("La edad de la persona es: " + personaUno.getEdad()); // GET -> OBTENGO LA EDAD
+
+
+            Botella botellaUno = new Botella("Rojo","Plastico"); // new (palabra reservada) para invocar al constructor de la clase
+            botellaUno.Capacidad = 200;
+
+            Botella botellaDos = new Botella();
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
