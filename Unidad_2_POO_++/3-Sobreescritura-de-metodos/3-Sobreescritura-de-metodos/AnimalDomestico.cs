060105da072668a7f80d30647ff2a5e5f63a6e47
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-31 16:27:16 -0300

    Sobre escritura de metodos PADRE -> HIJO

---
+++ Unidad_2_POO_++/3-Sobreescritura-de-metodos/3-Sobreescritura-de-metodos/AnimalDomestico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Sobreescritura_de_metodos
+{
+    class AnimalDomestico:Animal
+    {
+        public string Nombre { get; set; }
+
+        public override string ToString()       // override -> SOBRE ESCRIBIR
+        {
+            return "Animal Domestico: " + Nombre;
+        }
+
+    }
+}
