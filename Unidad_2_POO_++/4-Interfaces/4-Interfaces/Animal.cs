c5d12f9363407a9f2ee4d9709add70075475eb0d
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-31 16:57:54 -0300

    Implementacion de Interfaces

---
+++ Unidad_2_POO_++/4-Interfaces/4-Interfaces/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Interfaces
+{
+    class Animal
+    {
+
+
+        public virtual string comunicarse()     // virtual -> me permite que los hijos puedan reescribir el metodo
+        {
+            return "ruido, ruido";
+        }
+    }
+}
