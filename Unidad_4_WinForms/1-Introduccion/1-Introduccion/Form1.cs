b75da2cdf22e1d36519b1e7bf2dbdadf56ab85dd
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-02 18:36:09 -0300

    Intro a aplicaciones .net con winForms

---
+++ Unidad_4_WinForms/1-Introduccion/1-Introduccion/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_Introduccion
+{
+    public partial class Form1 : Form   // partial -> nos permite tener una clase por PARTES. CONTROL + CLICK sobre InitializeComponent y vemos la otra parte
+    {
+        public Form1()
+        {
+            InitializeComponent();  // METODO DE CLASE, inicializa los componentes del formulario
+        }
+
+
+    }
+}
