14c462fb49d2cb4bd65d7cb32387a021a72ec843
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-02 23:13:10 -0300

    Menus y navegacion entre ventanas

---
+++ Unidad_4_WinForms/4-Menu-Navegacion/4-Menu-Navegacion/frmPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_Menu_Navegacion
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void perfilPersonaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void tsbPerfilPersonal_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
