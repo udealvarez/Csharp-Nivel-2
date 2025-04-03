c92c8a516d1d5849129a259daaaaeac844397905
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-03 19:53:19 -0300

    Practica 1 - Mi primera app - Edicion de formulario

---
+++ Unidad_4_WinForms/Practica-1-Form/Practica-1-Form/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1_Form
+{
+    public partial class FormularioPrincipal : Form
+    {
+        public FormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FormularioPrincipal_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenidos a C#");
+        }
+
+        private void FormularioPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Chau chau..");
+        }
+    }
+}
