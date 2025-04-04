1ab0d51225e30e465907fdbd41215f7f61499dac
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-04 10:24:02 -0300

    Control TextBox -> PROP & EVENTOS

---
+++ Unidad_4_WinForms/Practica-4-Control-TextBox/Practica-4-Control-TextBox/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Practica_4_Control_TextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnBoton = new System.Windows.Forms.Button();
+            this.txtNuevo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(239, 139);
+            this.txtApellido.MaxLength = 11;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(183, 20);
+            this.txtApellido.TabIndex = 0;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
+            // 
+            // btnBoton
+            // 
+            this.btnBoton.BackColor = System.Drawing.Color.Yellow;
+            this.btnBoton.Font = new System.Drawing.Font("ROG Fonts", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBoton.Image = global::Practica_4_Control_TextBox.Properties.Resources.icons8_marca_de_verificación_de_instagram_windows_11_color_32;
+            this.btnBoton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBoton.Location = new System.Drawing.Point(239, 290);
+            this.btnBoton.Name = "btnBoton";
+            this.btnBoton.Size = new System.Drawing.Size(183, 43);
+            this.btnBoton.TabIndex = 1;
+            this.btnBoton.Text = "Click";
+            this.btnBoton.UseVisualStyleBackColor = false;
+            this.btnBoton.Click += new System.EventHandler(this.btnBoton_Click);
+            // 
+            // txtNuevo
+            // 
+            this.txtNuevo.Location = new System.Drawing.Point(239, 189);
+            this.txtNuevo.Multiline = true;
+            this.txtNuevo.Name = "txtNuevo";
+            this.txtNuevo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNuevo.Size = new System.Drawing.Size(183, 82);
+            this.txtNuevo.TabIndex = 2;
+            this.txtNuevo.Leave += new System.EventHandler(this.txtNuevo_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 430);
+            this.Controls.Add(this.txtNuevo);
+            this.Controls.Add(this.btnBoton);
+            this.Controls.Add(this.txtApellido);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Practica 4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnBoton;
+        private System.Windows.Forms.TextBox txtNuevo;
+    }
+}
+
---
+++ Unidad_4_WinForms/Practica-4-Control-TextBox/Practica-4-Control-TextBox/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_4_Control_TextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBoton_Click(object sender, EventArgs e)
+        {
+            if (txtApellido.Text == "")
+            {
+                // cambiar el color de fondo del TextBox si el TextBox se encuentra vació
+                txtApellido.BackColor = Color.Red;
+            }
+            else
+            {                
+                txtApellido.BackColor = System.Drawing.SystemColors.Control;
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)       // KeyPress ->  solo para ingresar Números
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNuevo_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tiene " + txtNuevo.Text.Length + " Caracteres");
+        }
+    }
+}
