6f2ee49e15fee6a6256dc96031d2edfaa1c3728f
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-03 22:59:20 -0300

    Practica 3 - Label & LinkLabel -> edicion de propiedades

---
+++ Unidad_4_WinForms/Practica-3-Control-Label-LinkLabel/Practica-3-Control-Label-LinkLabel/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3_Control_Label_LinkLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Cambiamos el color de la propiedad BackColor.
+            lblEtiqueta.BackColor = Color.Cyan;
+
+            // Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta.
+            lblEtiqueta.Cursor = Cursors.Hand;
+        }
+
+        private void lblEtiqueta_MouseLeave(object sender, EventArgs e)
+        {
+            // Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta.
+            lblEtiqueta.BackColor = System.Drawing.SystemColors.Control;
+
+            // Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta.
+            lblEtiqueta.Cursor = Cursors.Arrow;
+        }
+    }
+}
