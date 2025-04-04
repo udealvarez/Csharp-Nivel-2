65418f3f62a29c920bb557e66b6ec145fd0a5d5e
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-04 15:14:10 -0300

    Carpeta Unidad_3_POO_++ con todas sus clases por orden

---
+++ Unidad_3_POO_++/3-Sobreescritura-de-metodos/3-Sobreescritura-de-metodos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Sobreescritura_de_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    AnimalDomestico a1 = new AnimalDomestico();
+            //   a1.Nombre = "Junior";
+
+            Gato g1 = new Gato();
+            g1.Nombre = "pepe";
+
+            Perro p1 = new Perro();
+            p1.Nombre = "John";
+            
+
+            List<Animal> animales = new List<Animal>();
+            animales.Add(p1);
+            animales.Add(new Pez());
+            animales.Add(new Canario());
+            animales.Add(g1);
+            animales.Add(new Aguila());
+            animales.Add(new Gato());
+
+
+            foreach (Animal animals in animales)
+            {
+                Console.WriteLine(animals.comunicarse());
+            }
+
+            // POLIMORFISMO: es la caracteristica que tienen los objetos de frente a un mismo estimulo comportarse de manera de distinta
+                                                                      // de frente a un mismo METODO....
+                                                                      // metodo -> comunicarse()
+
+            
+            Console.WriteLine(g1.comunicarse());
+            Console.WriteLine("El perro se comunica... " + p1.comunicarse());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
