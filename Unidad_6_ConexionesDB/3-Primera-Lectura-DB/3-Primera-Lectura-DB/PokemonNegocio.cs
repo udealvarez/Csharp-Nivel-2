0e0080b9e859771f21987a801a0b9c06d3d41863
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-08 12:18:05 -0300

    FEAT: primera lectura a la base de datos en este curso

---
+++ Unidad_6_ConexionesDB/3-Primera-Lectura-DB/3-Primera-Lectura-DB/PokemonNegocio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;    // DECLARAR LA LIBRERIA
+
+namespace _3_Primera_Lectura_DB
+{
+    class PokemonNegocio
+    {
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection connection = new SqlConnection();
+            SqlCommand command = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                connection.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true;";
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT Numero, Nombre, Descripcion FROM POKEMONS";
+                command.Connection = connection;
+
+                connection.Open();      // abro la coneccion
+                lector = command.ExecuteReader();    // estoy haciendo una lectura
+
+                while (lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                connection.Close();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+    }
+}
