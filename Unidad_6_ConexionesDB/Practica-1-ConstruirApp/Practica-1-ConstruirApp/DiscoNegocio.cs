807de19807787163ca4a311957c984b7dc955d03
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-08 18:16:56 -0300

    feat: lecttura a la base + primera parte de app

---
+++ Unidad_6_ConexionesDB/Practica-1-ConstruirApp/Practica-1-ConstruirApp/DiscoNegocio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Practica_1_ConstruirApp
+{
+    class DiscoNegocio
+    {
+        public List<Disco> listar()
+        {
+            List<Disco> lista = new List<Disco>();
+            SqlConnection connection = new SqlConnection();
+            SqlCommand command = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+               // connection.ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=DISCOS_DB;Integrated Security=True";
+
+                connection.ConnectionString = "server=.\\SQLEXPRESS; database=DISCOS_DB; integrated security=true;";
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT D.Titulo, D.CantidadCanciones as Cantidad_Canciones, D.UrlImagenTapa, e.Descripcion as Estilo, t.Descripcion as Edicion FROM DISCOS D, ESTILOS E, TIPOSEDICION T WHERE e.Id = d.IdEstilo AND t.Id = D.IdTipoEdicion";
+                command.Connection = connection;
+
+                connection.Open();
+
+                lector = command.ExecuteReader();       // HAGO LA LECTURA
+
+                while (lector.Read())
+                {
+                    Disco auxiliar = new Disco();
+
+                    auxiliar.Titulo = (string)lector["Titulo"];
+                    auxiliar.CantidadCanciones = (int)lector["Cantidad_Canciones"];     // MEJOR VERSION
+                    auxiliar.UrlImagenTapa = (string)lector["UrlImagenTapa"];
+
+                    auxiliar.Estilo = new Elemento();
+                    auxiliar.Edicion = new Elemento();
+
+                    auxiliar.Estilo.Descripcion = (string)lector["Estilo"];
+                    auxiliar.Edicion.Descripcion = (string)lector["Edicion"];
+
+                    lista.Add(auxiliar);
+                }
+
+                connection.Close();
+                return lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
