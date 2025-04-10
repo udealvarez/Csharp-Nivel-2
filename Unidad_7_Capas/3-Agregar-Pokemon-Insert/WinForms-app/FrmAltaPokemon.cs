cea572211fdb72430c5ea7f45ffe50fa5650d397
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-09 21:42:15 -0300

    feat: agregar pokemon a la base, insert simple

---
+++ Unidad_7_Capas/3-Agregar-Pokemon-Insert/WinForms-app/FrmAltaPokemon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+
+namespace WinForms_app
+{
+    public partial class FrmAltaPokemon : Form
+    {
+        public FrmAltaPokemon()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            Pokemon poke = new Pokemon();
+
+            try
+            {
+                poke.Numero = Convert.ToInt32(txtNumero.Text);
+                poke.Nombre = txtNombre.Text;
+                poke.Descripcion = txtDescripcion.Text;
+
+                PokemonNegocio negocio = new PokemonNegocio();
+                negocio.agregar(poke);
+
+                MessageBox.Show("Agregado exitosamente", "ATENCION");
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
