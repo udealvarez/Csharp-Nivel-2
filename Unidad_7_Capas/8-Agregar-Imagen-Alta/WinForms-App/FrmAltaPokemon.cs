18a08a8bebf35721049140e8e9162bee25669878
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-23 15:09:37 -0300

    feat: Agregar imagen a la app + DB actualizada

---
+++ Unidad_7_Capas/8-Agregar-Imagen-Alta/WinForms-App/FrmAltaPokemon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+namespace WinForms_App
+{
+    public partial class FrmAltaPokemon : Form
+    {
+        public FrmAltaPokemon()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAltaPokemon_Load(object sender, EventArgs e)
+        {
+            ElementoNegocio elementoNegocio = new ElementoNegocio();
+
+            try
+            {
+                cbTipo.DataSource = elementoNegocio.listar();
+                cbDebilidad.DataSource = elementoNegocio.listar();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            Pokemon poke = new Pokemon();
+
+            try
+            {
+                poke.Numero = Convert.ToInt32(txtNumero.Text);
+                poke.Nombre = txtNombre.Text;
+                poke.Descripcion = txtDescripcion.Text;
+                poke.UrlImagen = txtUrlImagen.Text;
+                poke.Tipo = (Elemento)cbTipo.SelectedItem;
+                poke.Debilidad = (Elemento)cbDebilidad.SelectedItem;
+
+                PokemonNegocio negocio = new PokemonNegocio();
+                negocio.agregar(poke);
+
+                MessageBox.Show("Agregado exitosamente", "ATENCION");
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUrlImagen_Leave(object sender, EventArgs e) // propiedad del textbox URL IMAGEN -> LEAVE
+        {
+            cargarImagen(txtUrlImagen.Text);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbPokemon.Load("https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png");
+            }
+
+            // URL DEL POKEMON ELIMINADO    - >    https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png
+        }
+
+
+        /*
+         * picture box 
+         *      ziseMode -> strechtimage (ajusta la imagen al picture box)
+         */
+    }
+}
