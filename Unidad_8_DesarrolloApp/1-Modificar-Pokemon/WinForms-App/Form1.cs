0f36322a6b4da1d7ef38ce1fee8045a75e90fbc2
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-29 14:42:07 -0300

    feat: modificar pokemon + db actualizada

---
+++ Unidad_8_DesarrolloApp/1-Modificar-Pokemon/WinForms-App/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+
+namespace WinForms_App
+{
+    public partial class Form1 : Form
+    {
+        private List<Pokemon> listaPokemon;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void cargar()
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listar();
+
+            dgvPokemons.DataSource = listaPokemon;
+            dgvPokemons.Columns["UrlImagen"].Visible = false;
+            dgvPokemons.Columns["Id"].Visible = false;
+
+            pbPokemon.Load(listaPokemon[0].UrlImagen);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            FrmAltaPokemon alta = new FrmAltaPokemon();     // en el agregar llamo al constructor vacio
+            alta.ShowDialog();
+
+            // actulizar la carga del nuevo pokemon
+            cargar();
+        }
+
+        private void dgvPokemons_SelectionChanged(object sender, EventArgs e)
+        {
+            Pokemon pokemonSeleccionado = (Pokemon)dgvPokemons.CurrentRow.DataBoundItem;       // de la grilla, de la fila actual, dame el objeto enlazado                        
+            cargarImagen(pokemonSeleccionado.UrlImagen);
+
+            /*
+             *  PROPIEDADES DE DATAGRID
+             *  
+             *  SELECCIONAR UNA SOLA FILA
+             *  prop del data grid
+             *      selection mode -> fullrowselect
+             *      
+             *  EVITAR EDITAR CELDA INDIVIDUAL
+             *  prop del data grid
+             *      editmode -> editprogrammatically
+             *      
+             *  EVITAR SELECCION MULTIPLE DE FILAS
+             *  prop del data grid
+             *      multiselect -> false
+             */
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbPokemon.Load("https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png");
+            }
+
+            // URL DEL POKEMON ELIMINADO    - >    https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            // aca selecciono el objeto pokemon de la grilla
+            Pokemon seleccionado;
+            seleccionado = (Pokemon)dgvPokemons.CurrentRow.DataBoundItem; 
+
+            FrmAltaPokemon modificar = new FrmAltaPokemon(seleccionado);// por parametro le tengo que pasar el objeto pokemon que quiero modificar
+            modificar.ShowDialog();                             // LLAMO AL CONSTRUCTOR DEL FRM QUE TIENE COMO OBJETO UN POKEMON
+
+            // actulizar la carga del nuevo pokemon
+            cargar();
+        }
+    }
+}
---
+++ Unidad_8_DesarrolloApp/1-Modificar-Pokemon/WinForms-App/FrmAltaPokemon.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+
+namespace WinForms_App
+{
+    public partial class FrmAltaPokemon : Form
+    {
+        private Pokemon pokemon = null;
+
+        public FrmAltaPokemon()
+        {
+            InitializeComponent();
+        }
+
+        public FrmAltaPokemon(Pokemon pokemon)
+        {
+            InitializeComponent();
+            this.pokemon = pokemon;
+            Text = "Modificar Pokemon";     // modifica el nombre del formulario en el formulario de modificar
+
+            /*
+             *      this.pokemon = linea 18
+             *      pokemon de linea 28 hace referencia al pokemon del parametro             * 
+             */
+        }
+
+
+
+        private void FrmAltaPokemon_Load(object sender, EventArgs e)
+        {
+            ElementoNegocio elementoNegocio = new ElementoNegocio();
+
+            try
+            {
+                cbTipo.DataSource = elementoNegocio.listar();
+                // estas 2 lineas te permite que cuando quieras modificar tomes el id y la descripcion
+                cbTipo.ValueMember = "Id";
+                cbTipo.DisplayMember = "Descripcion";
+
+                cbDebilidad.DataSource = elementoNegocio.listar();
+                cbDebilidad.ValueMember = "Id";
+                cbDebilidad.DisplayMember = "Descripcion";
+
+                // pre cargar los datos en las cajas de texto
+                if (pokemon != null)// si es distinto de nulo -> tengo un pokemon para MODIFICAR
+                {
+                    txtNumero.Text = pokemon.Numero.ToString(); // ToString() -> por que es un numero
+                    txtNombre.Text = pokemon.Nombre;
+                    txtDescripcion.Text = pokemon.Descripcion;
+
+                    txtUrlImagen.Text = pokemon.UrlImagen;
+                    cargarImagen(pokemon.UrlImagen);
+
+                    cbTipo.SelectedValue = pokemon.Tipo.Id; // SelectedValue -> el valor seleccionado por el ID (linea 46)
+                    cbDebilidad.SelectedValue = pokemon.Debilidad.Id;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            PokemonNegocio negocio = new PokemonNegocio();
+
+            try
+            {
+                if (pokemon == null) // quiere decir que toque AGREGAR y NO modificar
+                {
+                    pokemon = new Pokemon();
+                }
+
+                pokemon.Numero = Convert.ToInt32(txtNumero.Text);
+                pokemon.Nombre = txtNombre.Text;
+                pokemon.Descripcion = txtDescripcion.Text;
+                pokemon.UrlImagen = txtUrlImagen.Text;
+                pokemon.Tipo = (Elemento)cbTipo.SelectedItem;
+                pokemon.Debilidad = (Elemento)cbDebilidad.SelectedItem;
+
+                if (pokemon.Id != 0)
+                {
+                    negocio.modificar(pokemon);
+                    MessageBox.Show("Modificado exitosamente", "ATENCION");
+                }
+                else
+                {
+                    negocio.agregar(pokemon);
+                    MessageBox.Show("Agregado exitosamente", "ATENCION");
+                }            
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUrlImagen_Leave(object sender, EventArgs e)// propiedad del textbox URL IMAGEN -> LEAVE
+        {
+            cargarImagen(txtUrlImagen.Text);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbPokemon.Load("https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png");
+            }
+
+            // URL DEL POKEMON ELIMINADO    - >    https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png
+        }
+
+        /*
+        * picture box 
+        *      ziseMode -> strechtimage (ajusta la imagen al picture box)
+        */
+    }
+}
