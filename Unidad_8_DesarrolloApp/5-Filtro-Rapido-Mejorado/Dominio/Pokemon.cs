d0881f56372b71815587fd4e13d8ab7a68a6b52f
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-30 11:20:03 -0300

    feat: filtro rapido mejorado sin usar boton

---
+++ Unidad_8_DesarrolloApp/5-Filtro-Rapido-Mejorado/Dominio/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        // ANOTATION -> sirve para validaciones, formato de fecha, sirve para darle un nombre a la columna del datagrid
+
+        [DisplayName("Número")]
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+
+        public Elemento Tipo { get; set; }
+        public Elemento Debilidad { get; set; }
+    }
+}
