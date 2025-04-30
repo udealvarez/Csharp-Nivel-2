243c993953ae7ccd89d35a579c9955eaf14163e5
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-30 10:50:40 -0300

    feat: filtro rapido por busqueda

---
+++ Unidad_8_DesarrolloApp/4-Filtro-Rapido/Negocio/PokemonNegocio.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class PokemonNegocio
+    {
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection connection = new SqlConnection();
+            SqlCommand command = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                connection.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true;";
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT Numero, Nombre, p.Descripcion, UrlImagen, e.Descripcion AS Tipo, p.IdTipo, p.IdDebilidad , d.Descripcion as Debilidad, p.Id FROM POKEMONS p, ELEMENTOS e, ELEMENTOS d WHERE e.Id = p.IdTipo AND d.Id =p.IdDebilidad and p.Activo = 1";
+                command.Connection = connection;
+
+                connection.Open();
+                lector = command.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Id = (int)lector["Id"];
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    if (!(lector["UrlImagen"] is DBNull))   // esto es mas practico
+                    {
+                        aux.UrlImagen = (string)lector["UrlImagen"];
+                    }
+
+                    aux.Tipo = new Elemento();
+                    aux.Tipo.Id = (int)lector["IdTipo"];
+                    aux.Tipo.Descripcion = (string)lector["Tipo"];
+
+                    aux.Debilidad = new Elemento();
+                    aux.Debilidad.Id = (int)lector["IdDebilidad"];
+                    aux.Debilidad.Descripcion = (string)lector["Debilidad"];
+
+
+                    lista.Add(aux);
+                }
+                connection.Close();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void agregar(Pokemon nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO POKEMONS (Numero, Nombre, Descripcion, Activo, IdTipo, IdDebilidad, UrlImagen) values (" + nuevo.Numero + ",'" + nuevo.Nombre + "','" + nuevo.Descripcion + "',1, @idTipo, @idDebilidad, @UrlImagen)");
+                datos.setearParametro("@idTipo", nuevo.Tipo.Id);
+                datos.setearParametro("@idDebilidad", nuevo.Debilidad.Id);
+                datos.setearParametro("@UrlImagen", nuevo.UrlImagen);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Pokemon poke)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("UPDATE POKEMONS SET Numero = @Numero, Nombre = @nombre, Descripcion = @desc, UrlImagen = @img, IdTipo = @idTipo, IdDebilidad = @idDebilidad WHERE Id = @id");
+
+                datos.setearParametro("@Numero", poke.Numero);
+                datos.setearParametro("@nombre", poke.Nombre);
+                datos.setearParametro("@desc", poke.Descripcion);
+                datos.setearParametro("@img", poke.UrlImagen);
+                datos.setearParametro("@idTipo", poke.Tipo.Id);
+                datos.setearParametro("@idDebilidad", poke.Debilidad.Id);
+                datos.setearParametro("@id", poke.Id);
+
+                datos.ejecutarAccion();     // -> se ejecuta la accion por que es un UPDATE
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("delete from POKEMONS where Id = @id");
+
+                datos.setearParametro("@id", id);
+
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminarLogico(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("UPDATE POKEMONS SET Activo = 0 WHERE ID = @id");
+
+                datos.setearParametro("@id", id);
+
+                datos.ejecutarAccion();     // -> se ejecuta la accion por que es un UPDATE
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ Unidad_8_DesarrolloApp/4-Filtro-Rapido/WinForms-App/FrmAltaPokemon.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+namespace WinForms_App
+{
+    public partial class FrmAltaPokemon : Form
+    {
+        private Pokemon pokemon = null;
+
+        public FrmAltaPokemon()
+        {
+            InitializeComponent();
+        }
+
+        public FrmAltaPokemon(Pokemon pokemon)
+        {
+            InitializeComponent();
+            this.pokemon = pokemon;
+            Text = "Modificar Pokemon";     // modifica el nombre del formulario en el formulario de modificar
+        }
+
+        private void FrmAltaPokemon_Load(object sender, EventArgs e)
+        {
+            ElementoNegocio elementoNegocio = new ElementoNegocio();
+
+            try
+            {
+                cbTipo.DataSource = elementoNegocio.listar();
+                // estas 2 lineas te permite que cuando quieras modificar tomes el id y la descripcion
+                cbTipo.ValueMember = "Id";
+                cbTipo.DisplayMember = "Descripcion";
+
+                cbDebilidad.DataSource = elementoNegocio.listar();
+                cbDebilidad.ValueMember = "Id";
+                cbDebilidad.DisplayMember = "Descripcion";
+
+                // pre cargar los datos en las cajas de texto
+                if (pokemon != null)// si es distinto de nulo -> tengo un pokemon para MODIFICAR
+                {
+                    txtNumero.Text = pokemon.Numero.ToString(); // ToString() -> por que es un numero
+                    txtNombre.Text = pokemon.Nombre;
+                    txtDescripcion.Text = pokemon.Descripcion;
+
+                    txtUrlImagen.Text = pokemon.UrlImagen;
+                    cargarImagen(pokemon.UrlImagen);
+
+                    cbTipo.SelectedValue = pokemon.Tipo.Id; // SelectedValue -> el valor seleccionado por el ID (linea 46)
+                    cbDebilidad.SelectedValue = pokemon.Debilidad.Id;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbPokemon.Load("https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png");
+            }
+
+            // URL DEL POKEMON ELIMINADO    - >    https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png
+
+            /*
+            *   picture box 
+            *      ziseMode -> strechtimage (ajusta la imagen al picture box)
+            */
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUrlImagen_Leave(object sender, EventArgs e)
+        {
+            cargarImagen(txtUrlImagen.Text);
+        }
+    }
+}
